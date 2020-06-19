a41a6fd9ca9a73578583759e01d27d198fedcf21
Chitoku <odango@chitoku.jp>
2020-06-19 12:25:36 +0900

    Put pseudo git

--- 100-prerun.sh
+++ 100-prerun.sh
@@ -20,6 +20,12 @@ if [[ ! -f $done_file ]]; then
   PATH=$XXH_SHELL_ZSH/zsh-bin/bin:$PATH \
     $XDG_CACHE_HOME/antigen/bundles/junegunn/fzf/install --all --xdg --no-update-rc > /dev/null
 
+  # pseudo git for antigen
+  if ! type git &> /dev/null; then
+    echo "#!/bin/bash\nexit 1" > $XXH_SHELL_ZSH/zsh-bin/bin/git
+    chmod +x $XXH_SHELL_ZSH/zsh-bin/bin/git
+  fi
+
   cp $XXH_HOME/.zshenv $XXH_SHELL_ZSH
   touch $done_file
 fi
